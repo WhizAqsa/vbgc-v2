c715365d28c64225f234600ab02a8338bcc88731
WhizAqsa <abatool.bscs22seecs@seecs.edu.pk>
2026-04-13 18:17:42 +0500

    feat: apply on click for login

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -1,6 +1,15 @@
+"use client";
+
+
 import { FaGlobe } from 'react-icons/fa';
+import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
+    const router = useRouter();
+
+    const handleLogin = () => {
+        router.push('/login');
+    };
     return (
         <header className="fixed right-0 top-0 z-30 w-full bg-gradient-to-br from-zinc-800 via-slate-950 to-black pl-[84px] text-white">
             <div className="flex items-center justify-end gap-3 px-6 py-4 sm:gap-6 sm:px-8 sm:py-5">
@@ -12,6 +21,8 @@ export default function Navbar() {
                     English
                 </button>
                 <button
+                    onClick={handleLogin}
+
                     type="button"
                     className="rounded-full px-5 py-2.5 text-sm font-semibold text-white transition hover:bg-white/10"
                 >
